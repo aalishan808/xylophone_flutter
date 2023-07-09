2796fecba13055acf1ffe1da461d0587adb0535e
aalishan808 <92679900+aalishan808@users.noreply.github.com>
2023-07-09 13:45:43 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
-
   void playit(int num) {
-  final player = AudioPlayer();
-  player.play(
-      AssetSource('note$num.wav')
-  ); // will immediately start playing
-}
+    final player = AudioPlayer();
+
+    player.play(AssetSource('$num.wav'));
+    // will immediately start playing
+  }
+
 //function to make the keys
- Expanded makekey(int num, Color color){
-  return Expanded(
-    child: TextButton(
-
-      onPressed: () {
-        playit(num);// will immediately start playing
-      },
-      style: TextButton.styleFrom(
-        backgroundColor: color,
+  Expanded makekey(int num, Color color, String note) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playit(num); // will immediately start playing
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+        child: Center(child: Text("$note")),
       ),
+    );
+  }
 
-      child: Center(),
-
-    ),
-  );
-}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-
-            backgroundColor: Colors.black,
+        backgroundColor: Colors.black,
         body: SafeArea(
-            child:Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                makekey(1,Colors.blue),
-                makekey(2,Colors.red),
-                makekey(3,Colors.teal),
-                makekey(4,Colors.orange),
-                makekey(5,Colors.green),
-                makekey(6,Colors.yellow),
-                makekey(7,Colors.purple),
-
-
-              ],
-            )
-
-        ),
-
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            makekey(1, Colors.blue,   "C    SA"),
+            makekey(2, Colors.green,  "D    RE"),
+            makekey(3, Colors.yellow, "E    GA"),
+            makekey(4, Colors.brown,  "F    MA"),
+            makekey(5, Colors.orange, "G    PA"),
+            makekey(6, Colors.red,    "A    DHA"),
+            makekey(7, Colors.teal,   "B    NI"),
+            makekey(7, Colors.blue,   "C    sa"),
+            makekey(7, Colors.green,  "D    re"),
+            makekey(7, Colors.yellow, "E    ga"),
+          ],
+        )),
       ),
-
     );
-
   }
-
 }
